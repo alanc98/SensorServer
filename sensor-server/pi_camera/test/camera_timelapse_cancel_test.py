aa51c9f75081070b6a709f02d2944053cfb8e252
alanc98 <Alan.Cudmore@gmail.com>
2018-03-26 22:38:58 -0400

    added video and timelapse cancel tests

---
+++ sensor-server/pi_camera/test/camera_timelapse_cancel_test.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+import zmq
+import time
+import sys
+
+#
+# Helper Function to setup a ZMQ subscription
+#
+def SubscribeToFilter(FilterText, SubSocket):
+   if isinstance(FilterText, bytes):
+      FilterText = FilterText.decode('ascii')
+   SubSocket.setsockopt_string(zmq.SUBSCRIBE, FilterText)
+
+print("Connecting to Sensor Server")
+sys.stdout.flush()
+context = zmq.Context()
+#  Socket to talk to server
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://127.0.0.1:5557")
+# socket.connect("tcp://192.168.0.246:5557")
+
+# SUB socket to get status data
+sub_socket = context.socket(zmq.SUB)
+sub_socket.connect('tcp://localhost:5558') 
+# sub_socket.connect('tcp://192.168.0.246:5558') 
+SubscribeToFilter('SENSOR_PUB',sub_socket)
+
+print("Connected")
+socket.send("SENSOR_REQ,DEV=PI_CAMERA,SUB_DEV=TIMELAPSE,CMD=CAPTURE,SIZE=1,VFLIP=TRUE,FILE_PRE=/home/pi/Pictures/timelapse-,DELAY=1,FRAMES=10,SENSOR_REQ_END")
+message = socket.recv()
+print("Received Pi_Camera sensor reply:",message)
+
+time.sleep(2)
+
+#
+# Cancel the timelapse
+# 
+socket.send("SENSOR_REQ,DEV=PI_CAMERA,SUB_DEV=TIMELAPSE,CMD=CANCEL,SENSOR_REQ_END")
+message = socket.recv()
+print("Received Pi_Camera sensor reply:",message)
+
